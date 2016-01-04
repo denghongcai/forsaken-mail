091912db3f3fdde83ddc93d671a3a0a45bda00f7
Hongcai Deng <admin@dhchouse.com>
2016-01-04 10:38:37 +0800

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let app = express();
 
 app.set('x-powered-by', false);
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 3600000}));
 
 app.use('/api', api);
 
